7ed5d8a22024f574b2d6a441eb270e9a7d09abb2
flynncao <flynncao@yahoo.com>
2024-02-05 20:11:08 +0800

    feat: introduce typegoose

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,4 +1,4 @@
-import { Schema, model } from 'mongoose'
+import { getModelForClass, prop } from '@typegoose/typegoose'
 
 export interface IPost {
   id: number
@@ -9,42 +9,64 @@ export interface IPost {
   updated_at: Date | null
 }
 
-const postSchema = new Schema<IPost>({
-  id: {
-    type: Number,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-    default: '',
-  },
-  author: {
-    type: String,
-    required: true,
-    default: '',
-  },
-  created_at: {
-    type: Date,
-    required: true,
-    default: new Date(),
-  },
-  updated_at: {
-    type: Date,
-    required: false,
-    default: null,
-  },
-})
+export class Post {
+  @prop({ required: true, unique: true, index: true })
+  public id!: number
 
-const User = model<IPost>('Post', postSchema)
+  @prop({ required: true })
+  public title!: string
+
+  @prop({ required: false, default: '' })
+  public content!: string
+
+  @prop({ required: false, default: '' })
+  public author!: string
+
+  @prop({ required: false, default: () => new Date() })
+  public created_at!: Date
+
+  @prop({ required: false, default: null })
+  public updated_at?: Date
+}
+
+// const postSchema = new Schema<IPost>({
+//   id: {
+//     type: Number,
+//     required: true,
+//   },
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   content: {
+//     type: String,
+//     required: true,
+//     default: '',
+//   },
+//   author: {
+//     type: String,
+//     required: true,
+//     default: '',
+//   },
+//   created_at: {
+//     type: Date,
+//     required: true,
+//     default: new Date(),
+//   },
+//   updated_at: {
+//     type: Date,
+//     required: false,
+//     default: null,
+//   },
+// })
+
+// const User = model<IPost>('Post', postSchema)
+
+const PostModel = getModelForClass(Post)
 
 // TODO: Use menu to operate database
 export function findOrCreateUser(id: number) {
-  return User.findOneAndUpdate(
+  return PostModel.findOneAndUpdate(
     // filter
     { id },
     // update
@@ -59,7 +81,7 @@ export function findOrCreateUser(id: number) {
 export function editPost(id: number, content: string, updated_at?: Date) {
   if (!updated_at)
     updated_at = new Date()
-  return User.findOneAndUpdate(
+  return PostModel.findOneAndUpdate(
     { id },
     { content, updated_at },
     {
